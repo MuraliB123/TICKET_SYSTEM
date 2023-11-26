f3c9b573fc44529502b0028b355003af986fefa7
MuraliB123 <muralibabu1729@gmail.com>
2023-11-26 21:05:14 +0530

    initial

---
+++ app.py
@@ -0,0 +1,64 @@
+import MySQLdb
+from flask import Flask, render_template, request, session
+app = Flask(__name__)
+from flask import jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__, static_folder='static')
+app.secret_key = 'mykey'
+engine = create_engine('mysql+pymysql://root:new_password@localhost/ticket_system')
+Session = sessionmaker(bind=engine)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:new_password@localhost/ticket_system'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+from models import  db, UserLogin,Ticket
+db.init_app(app)
+assign_id = 0
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        userid = request.form['userid']
+        password = request.form['password']
+        user = UserLogin.query.filter_by(id=userid, password=password).first()
+
+        if user:
+            data = "success"
+            session['user_id'] = user.id
+            return render_template('user_login.html',data=data)
+            
+        else:
+            data = "Login Failed"
+            return render_template('user_login.html',data = data)
+
+    return render_template('user_login.html')
+
+@app.route('/raise',methods=['GET','POST'])
+def assign():
+    if request.method == 'POST':
+        raised_by = session['user_id']
+        issue = request.form['issue']
+        assign_to = get_assign_id()
+        new_ticket = Ticket(issue_description=issue, raised_by=raised_by, assigned_to=assign_to)
+        db.session.add(new_ticket)
+        db.session.commit()
+        message   = "your ticket has been raised"
+        return render_template('ticket_raise.html',data = message)
+    return render_template('ticket_raise.html')
+
+
+
+
+def get_assign_id():
+    global assign_id
+    assign_id += 1
+    if assign_id > 5:
+        assign_id = 1
+    return assign_id
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class UserLogin(db.Model):
+    __tablename__ = 'user_login'
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(30), nullable=True)
+
+class Ticket(db.Model):
+    __tablename__ = 'tickets'
+    ticket_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    issue_description = db.Column(db.Text, nullable=True)
+    assigned_to = db.Column(db.Integer, db.ForeignKey('user_login.id'), nullable=True)
+    raised_by = db.Column(db.Integer, db.ForeignKey('user_login.id'), nullable=True)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(255), nullable=True)
